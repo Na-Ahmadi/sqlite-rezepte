b360dfea29f5a8be8693d5caae4a3951895aa2e8
Naser Ahmadi <ahmadi@neoskop.de>
2025-11-28 10:12:56 +0100

    Add URLPattern to server.js routing.

--- backend/routes/rezepteRouter.js
+++ backend/routes/rezepteRouter.js
@@ -1,17 +1,24 @@
+// import URLPattern from ''
+
 import {
   getAllRezepte,
   getRezeptById,
 } from "../controllers/rezeptController.js";
 
+const alleRezeptePattern = new URLPattern({ pathname: `/api/rezepte` });
+const rezeptByIdPattern = new URLPattern({ pathname: `/api/rezepte/:id` });
+
 export default async function rezepteRouter(req, res) {
-  if (req.url === "/api/rezepte") {
+  console.log("url: ", req.url);
+
+  if (alleRezeptePattern.test(req.url)) {
     await getAllRezepte(req, res);
     return;
   }
-  if (
-    req.url.startsWith("/api/rezepte/" || req.url.startsWith("/api/rezepte"))
-  ) {
-    const rezeptId = req.url.split("/")[3];
+
+  const match = rezeptByIdPattern.exec(req.url);
+  if (match) {
+    const rezeptId = match.pathname.groups.id;
     await getRezeptById(req, res, rezeptId);
     return;
   }
