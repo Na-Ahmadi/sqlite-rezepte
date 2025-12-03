58546a6f820e8023d4301df5304f6bfbfea6c685
Naser Ahmadi <ahmadi@neoskop.de>
2025-12-03 14:55:48 +0100

    Add URLPattern.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,20 @@
-import http from "node:http";
+import { createServer } from "node:http";
+
 import recipesRouter from "./routes/recipesRouter.js";
 
+
 const PORT = process.env.PORT || 3005;
-const server = http.createServer(async (req, res) => {
+const recipePattern = new URLPattern({ pathname: "/api/recipes/:rest*" });
+const server = createServer(async (req, res) => {
   try {
-    if (req.url.startsWith("/api/recipes")) {
-      recipesRouter(req, res);
+   if (recipePattern.test(req.url)) {
+      await recipesRouter(req, res);
       return;
     }
 
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ error: "Endpoint not found" }));
+
   } catch (error) {
     res.writeHead(500, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ error: error.message }));
