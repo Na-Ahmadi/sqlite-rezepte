7c4127ae6a71223b35221ef299141d1043b22ae8
Naser Ahmadi <ahmadi@neoskop.de>
2025-12-10 09:35:40 +0100

    Create Select form.

--- app/src/components/Recipes.js
+++ app/src/components/Recipes.js
@@ -10,13 +10,24 @@ export default function Recipes({ recipes }) {
   <img src="/spaghetti-mit-bolognese.jpg" alt="spaghetti-mit-bolognese"/>
 
   <h1>Alle Rezepten</h1>
+   <form method="GET" action="/" class="sort-form">
+    <label for="sort">Sortieren nach:</label>
+    <select name="sort" id="sort" >
+      <option value="" >-- wählen --</option>
+      <option value="updated_desc">Neuestes zuerst</option>
+      <option value="updated_asc">Ältestes zuerst</option>
+      <option value="title_asc">Titel A–Z</option>
+      <option value="">Titel Z–A</option>
+    </select>
+  </form>
+
   ${recipes
     .map(
       (r) => /* html */ `
           <div class="card">
             <h2><a href="/recipes/${r.id}">${r.title}</a></h2>
             <p>${r.description}</p>
-            <span>${r.updated}</span>
+            <span>Aktualisiert: ${r.updated}</span>
           </div>
           `
     )
