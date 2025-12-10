379efa7d1392d67630c8fe0cedcff4601ed526a5
Naser Ahmadi <ahmadi@neoskop.de>
2025-12-10 11:03:49 +0100

    Rebuild recipes.

--- app/src/components/Recipes.js
+++ app/src/components/Recipes.js
@@ -9,29 +9,37 @@ export default function Recipes({ recipes }) {
   <img src="/pfannkuchen.jpg" alt="pfannkuchen"/>
   <img src="/spaghetti-mit-bolognese.jpg" alt="spaghetti-mit-bolognese"/>
 
-  <h1>Alle Rezepten</h1>
-   <form method="GET" action="/" class="sort-form">
-    <label for="sort">Sortieren nach:</label>
-    <select name="sort" id="sort" >
-      <option value="" >-- wählen --</option>
-      <option value="updated_desc">Neuestes zuerst</option>
-      <option value="updated_asc">Ältestes zuerst</option>
-      <option value="title_asc">Titel A–Z</option>
-      <option value="">Titel Z–A</option>
-    </select>
-  </form>
+  <section class="recipes-wrapper">
+      <h1>Alle Rezepten</h1>
+      <header class="recipes-header">
+        <a href="/new-recipe" class="add-btn">Rezept hinzufügen</a>
+        <form method="GET" action="/" class="sort-form">
+          <label for="sort">Sortieren nach:</label>
+          <select name="sort" id="sort">
+            <option value="">-- wählen --</option>
+            <option value="updated_desc">Neuestes zuerst</option>
+            <option value="updated_asc">Ältestes zuerst</option>
+            <option value="title_asc">Titel A–Z</option>
+            <option value="title_desc">Titel Z–A</option>
+          </select>
+        </form>
+      </header>
 
-  ${recipes
-    .map(
-      (r) => /* html */ `
-          <div class="card">
-            <h2><a href="/recipes/${r.id}">${r.title}</a></h2>
-            <p>${r.description}</p>
-            <span>Aktualisiert: ${r.updated}</span>
-          </div>
-          `
-    )
-    .join("")}
-      <a href="/new-recipe" class="back-btn">Rezept hinzufügen</a>
+      <div class="recipes-container">
+        ${recipes
+          .map(
+            (r) => /* html */ `
+              <article class="recipe-card">
+                <image src="/" alt="image"/>
+                <h2><a href="/recipes/${r.id}">${r.title}</a></h2>
+                <p>${r.description}</p>
+                <span>Aktualisiert: ${r.updated}</span>
+              </article>
+            `
+          )
+          .join("")}
+      </div>
+    </section>
+
       `;
 }
